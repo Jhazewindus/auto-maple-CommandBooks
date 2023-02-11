48e5fede1c17443858aec1d8041b53766a68a9e2
JHnl <45789844+Jhazewindus@users.noreply.github.com>
2023-02-11 10:38:04 +0100

    Added erda shower and lucid soul example code

---
+++ example codes/Lucid soul example.py
@@ -0,0 +1,22 @@
+class LucidSoul(Command):
+    """
+    Places 'Lucid Soul Summon' in a given direction, or towards the center of the map if
+    no direction is specified.
+    """
+
+    def __init__(self, direction=None):
+        super().__init__(locals())
+        if direction is None:
+            self.direction = direction
+        else:
+            self.direction = settings.validate_horizontal_arrows(direction)
+
+    def main(self):
+        if self.direction:
+            press(self.direction, 1, down_time=0.1, up_time=0.05)
+        else:
+            if config.player_pos[0] > 0.5:
+                press('left', 1, down_time=0.1, up_time=0.05)
+            else:
+                press('right', 1, down_time=0.1, up_time=0.05)
+        press(Key.LUCID_SOUL, 3)
